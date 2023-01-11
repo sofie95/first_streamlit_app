0b04455729325263d9061584ef7afaae58acc150
sofie95 <122372106+sofie95@users.noreply.github.com>
2023-01-11 12:21:26 +0100

    Update streamlit_app.py
    Set index as fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 #Get data
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 st.title('My Parents New Healthy Diner')
 
