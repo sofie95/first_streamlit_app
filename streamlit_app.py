4f49f41f3d6f938c24354d67a393d9b7bfbfb135
sofie95 <122372106+sofie95@users.noreply.github.com>
2023-01-11 11:30:58 +0100

    Update streamlit_app.py
    import pandas, get data, print dataframe in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+#Get data
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 st.title('My Parents New Healthy Diner')
 
@@ -13,3 +17,5 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
