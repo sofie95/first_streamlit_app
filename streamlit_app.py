515d39d0f3a93df1ee3f606654d69c7740c2ae11
sofie95 <122372106+sofie95@users.noreply.github.com>
2023-01-12 16:04:50 +0100

    Update streamlit_app.py
    add entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,10 @@ st.dataframe(fruits_to_show)
 
 
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize the data to a pandas dataframe and write it to screen 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
