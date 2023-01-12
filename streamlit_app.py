1641071f34628bb77697632b339329a06a79793a
sofie95 <122372106+sofie95@users.noreply.github.com>
2023-01-12 15:58:50 +0100

    Update streamlit_app.py
    Add header to new section
    and change fruityvise response to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
+
+st.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
