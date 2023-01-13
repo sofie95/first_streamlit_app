ef537fd62d89ef139f9b3310a00e8dc4f8697e56
sofie95 <122372106+sofie95@users.noreply.github.com>
2023-01-13 11:41:01 +0100

    Update streamlit_app.py
    re-write code order and add try catch and error

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,17 +31,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
-
+#New section to display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please selcet a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+exept URLError as e:
+  st.error()
 
-# normalize the data to a pandas dataframe and write it to screen 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
 
 #don't run past here while trouble shooting
 st.stop()
